303f2fd0ee22159f02c025992fe600b2ac3ade1a
Rayane <rmorslaoui@gmail.com>
2023-05-26 15:31:39 +0200

    Finalization

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+from builtins import print
+
 from pysnmp.hlapi import *
 from pysnmp.smi import builder, view
 from scapy.config import conf
@@ -6,43 +8,72 @@ from scapy.layers.inet import IP, UDP
 from scapy.layers.l2 import Ether
 from scapy.sendrecv import srp1
 
+def discover_topology(host, community):
+    notFound = 'No more variables left in this MIB View'
 
-def discover_topology(router_ip, community):
-    # Create an SNMP engine + Oid of Routing table
-    snmp_engine = SnmpEngine()
-    oid_routing_table = '1.3.6.1.2.1.4.21.1' # https://bestmonitoringtools.com/mibdb/mibdb_search.php?mib=RFC1213-MIB
+    oid_interfaces = '1.3.6.1.2.1.4.20.1.1' # https://bestmonitoringtools.com/mibdb/mibdb_search.php?mib=RFC1213-MIB
+    oid_next_hop = '1.3.6.1.2.1.4.24.4.1.4'
 
-    # Load MIB modules for routing table
-    mib_builder = builder.MibBuilder().loadModules('RFC1213-MIB')
-    mib_view_controller = view.MibViewController(mib_builder)
+    print("====================================================")
+    print(f"This is the topology from : ({host}):")
+    print("====================================================")
 
-    # Define the SNMP request to retrieve the routing table from the router
-    error_indication, error_status, error_index, var_binds = next(
-        getCmd(snmp_engine,
-               CommunityData(community, mpModel=1), #mpModel=1 for selecting the version 2c of the community
-               UdpTransportTarget((router_ip, 161)),
-               ContextData(),
-               ObjectType(ObjectIdentity(oid_routing_table)))
-    )
+    next_hop = ''
+    for (errorIndication, errorStatus, errorIndex, varBinds) in nextCmd(SnmpEngine(),
+                                                                        CommunityData(community),
+                                                                        UdpTransportTarget((host, 161)),
+                                                                        ContextData(),
+                                                                        ObjectType(ObjectIdentity(oid_interfaces)),
+                                                                        ObjectType(ObjectIdentity(oid_next_hop)),
+                                                                        lexicographicMode=False):
+        if errorIndication:
+            print(errorIndication)
+            break
+        elif errorStatus:
+            print('%s at %s' % (errorStatus.prettyPrint(),
+                                errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+            break
+        else:
+            for varBind in varBinds:
+                oid, value = varBind
+                #Continue if the oid is broadcast, and something we don't want, or no value in the result with this MIB oid.
+                if ('0.0.0.0' in value.prettyPrint()) or value.prettyPrint() == notFound:
+                    continue
 
-    # Process the response
-    if error_indication:
-        print(f'Error: {error_indication}')
-    elif error_status:
-        print(f'Error: {error_status.prettyPrint()} at {error_index}')
+                #Print the current intnerface
+                if oid_interfaces in str(oid):
+                    print(f'Interface : {value.prettyPrint()}')
+
+                #Save the nextHop if there is one.
+                if oid_next_hop in str(oid):
+                    next_hop = value.prettyPrint()
+
+    #Recursively method for nextHop if exist
+    if next_hop:
+        print(f'NextHop IP address is : {next_hop}')
+        discover_topology(next_hop, community)
     else:
-        # Print the topology information from the router
-        print(f"This is the topology from the router : ({router_ip}):")
-        for var_bind in var_binds:
-            oid, value = var_bind
-            table_entry = mib_view_controller.getNode(oid)
-            print(f'Topology: {table_entry.getDisplayHint().clone(value)}')
+        print('No NextHop IP address found')
 
-            # Recursively discover other routers in the current autonomous area
-            if "router" in str(table_entry):
-                next_hop_ip = value.prettyPrint()
-                discover_topology(next_hop_ip, community)
+# Function to extract dhcp_options by key
+def get_option(dhcp_options, key):
 
+    must_decode = ['hostname', 'domain', 'vendor_class_id']
+    try:
+        for i in dhcp_options:
+            if i[0] == key:
+                # If DHCP Server Returned multiple name servers
+                # return all as comma seperated string.
+                if key == 'name_server' and len(i) > 2:
+                    return ",".join(i[1:])
+                # domain and hostname are binary strings,
+                # decode to unicode string before returning
+                elif key in must_decode:
+                    return i[1].decode()
+                else:
+                    return i[1]
+    except:
+        pass
 
 # DHCP Part | Retrieve the IP address of the initial router
 conf.checkIPaddr = False
@@ -50,17 +81,8 @@ packet_dhcp_discover = Ether(dst="ff:ff:ff:ff:ff:ff") / IP(src="0.0.0.0", dst="2
 packet_dhcp_offer = srp1(packet_dhcp_discover, verbose=0)
 
 if packet_dhcp_offer and DHCP in packet_dhcp_offer and packet_dhcp_offer[DHCP].options[0][1] == 2: #2 == type Offer packet
-    router_ip = packet_dhcp_offer[BOOTP].siaddr
-    print(f"Initial router IP: {router_ip}") # 0.0.0.0
-
-    print("Offered IP address:", packet_dhcp_offer[IP].src)
-    print("Offered MAC address:", packet_dhcp_offer[Ether].src)
-    print("Server IP address:", packet_dhcp_offer[BOOTP].siaddr) # Only this one == 0.0.0.0
-    print("Server MAC address:", packet_dhcp_offer[BOOTP].chaddr)
-    print("Options:", packet_dhcp_offer[DHCP].options)
-
     community = 'public'  # SNMP community string
-    router_ip = '10.0.1.254'  # I put this for testing the code, because DHCP return 0.0.0.0 for the router_IP
+    router_ip =  get_option(packet_dhcp_offer[DHCP].options, 'router')
     discover_topology(router_ip, community)
 else:
     print("Failed to retrieve the initial router IP via DHCP.")
